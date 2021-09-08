e444a9c01ec9a6ed836eabf8da05a200fb9a3d6a
NguyenBaTuong <61742750+NguyenBaTuong@users.noreply.github.com>
2021-09-08 15:34:35 +0700

    dev home page - new

--- js/index.js
+++ js/index.js
@@ -36,3 +36,39 @@ function reset(){
         down[i].classList.remove("none");
     }
 }
+
+// nav__
+function compact() {
+    var check = document.getElementById('slider');
+    var element = document.getElementById('sliderxbox');
+    var title = document.getElementById('home__nav__i4');
+    var s = document.getElementsByClassName('home__nav-item');
+    var stt = document.getElementsByClassName('home__nav-item-title-icon');
+    var nav__scroll = document.getElementById('nav__scroll');
+    var body = document.getElementById('home__content');
+
+    if (check.checked == 0) {
+        element.classList.add('compact');
+        body.classList.add('compact');
+        for (let index = 1; index <= s.length; index++) {
+            document.getElementById('name'+index).style.display = 'none';
+        }
+        for (let index = 1; index <= stt.length; index++) {
+            document.getElementById('stt'+index).style.display = 'none';
+        }
+        title.style.display = 'none';
+        nav__scroll.style.padding = '100px 0 0 0';
+    } else {
+        body.classList.remove('compact');
+        element.classList.remove('compact');
+        for (let index = 1; index <= s.length; index++) {
+            document.getElementById('name'+index).style.display = 'unset';
+        }
+        for (let index = 1; index <= stt.length; index++) {
+            document.getElementById('stt'+index).style.display = 'unset';
+        }
+        title.style.display = 'flex';
+        nav__scroll.style.padding = 'unset';
+    }
+
+}
