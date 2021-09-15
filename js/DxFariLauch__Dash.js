b4a4cf39781d0dc10b1033189a88c9fba98a9aa5
NguyenBaTuong <61742750+NguyenBaTuong@users.noreply.github.com>
2021-09-15 09:05:05 +0700

    dev new DxSale Dao: fee + eth + lock

--- js/DxFariLauch__Dash.js
+++ js/DxFariLauch__Dash.js
@@ -94,28 +94,7 @@ function show__fourth() {
 
 }
 
-// time 
-function startTime() {
-    const today = new Date();
-    let d = today.getDate();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    var show__time = document.getElementById('back__time');
-    var time = `${d}:${h}:${m}:${s}`;
-    console.log(time);
-    show__time.innerHTML = time;
-    setTimeout(startTime, 1000);
-  }
-  
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i}; 
-    return i;
-  }
-
-//   copy lock 
+//copy
 function copy__lick__lock() {
     var copyText = document.getElementById("copy__lick__lock");
     copyText.select();
